dc8c79e0b21edc98a45b34cf7cf4bb4dbfe079e6
reg <entry.reg@gmail.com>
2014-08-22 20:33:11 +0400

    new version of main page

--- resources/main.js
+++ resources/main.js
@@ -1,49 +1,97 @@
 /*
- * Copyright (c) 2013 Developed by reg <entry.reg@gmail.com>
+ * Copyright (c) %year% Developed by reg <entry.reg@gmail.com>
  * 
  * Distributed under the MIT license
  * (see accompanying file LICENSE or a copy at http://opensource.org/licenses/MIT)
 */
 
-var Effects =   
-{
-    EXPLODE: 'explode',
-    DROP:    'drop',
-    BLIND:   'blind',
-    BOUNCE:  'bounce',
-    PUFF:    'puff'
-};
+'use strict';
 
-var EffectCfg = function(name, duration)
+var ShortInfoPrototype =
 {
-    this.name       = name;
-    this.duration   = duration;
-};
-
-var ShortInfo = function(cubes, sources)
-{
-    this.cubes      = cubes;
-    this.sources    = sources;
-    this.effect     = new EffectCfg(Effects.EXPLODE, 3120); //1750
-};
-
-ShortInfo.prototype.hide = function()
-{
-    this.cubes.hide();
-    this.sources.hide();
-};
-
-ShortInfo.prototype.show = function()
-{
-    this.cubes.draggable({ revert: true });
+    EffectType: 
+    {
+        EXPLODE: 'explode',
+        DROP:    'drop',
+        SLIDE:   'slide',
+        BLIND:   'blind',
+        BOUNCE:  'bounce',
+        PUFF:    'puff'
+    },
     
-    var sources = this.sources;
-    this.cubes.show(this.effect.name, {}, this.effect.duration, function(){
-        sources.show("slow");
-    });
+    cubes: null,
+    arrow: null,
+    code:  null,
+    
+    EffectCfg: function(name, duration, param)
+    {
+        this.name       = name;
+        this.duration   = duration;
+        this.param      = param;
+    },
+    
+    hide: function()
+    {
+        this.cubes._.hide();
+        this.arrow._.hide();
+    },
+    
+    show: function()
+    {
+        this.cubes._.draggable({ revert: true });
+        
+        var arrow = this.arrow;
+        this.cubes._.show(
+                this.cubes.effect.name, 
+                this.cubes.effect.param, 
+                this.cubes.effect.duration, 
+                function() {
+                    arrow._.show(
+                        arrow.effect.name, arrow.effect.param, arrow.effect.duration
+                    );
+                }
+        );
+    },
+    
+    attachArrowClick: function(target)
+    {
+        this.arrow._.click(function(e)
+        {
+            target._.hide(target.effect.name, {}, target.effect.duration);
+            
+            setTimeout(function(){
+                target._.show(target.effect.name, target.effect.param, target.effect.duration);
+            }, target.effect.param.delay);
+        });        
+    },
+    
+    detachArrowClick: function()
+    {
+        this.arrow._.off('click');
+    },
+    
+    construct: function(code, cubes, arrow)
+    {
+        this.cubes  = {
+            _: cubes,
+            effect: new this.EffectCfg(this.EffectType.EXPLODE, 3120, {}) //1750
+        };
+        
+        this.arrow  = {
+            _: arrow,
+            effect: new this.EffectCfg(this.EffectType.DROP, 750, {direction: 'right'})
+        };
+        
+        this.code   = {
+            _: code,
+            effect: new this.EffectCfg(this.EffectType.DROP, 750, {direction: 'right', delay: 4000})
+        };
+        
+        this.hide();
+        this.attachArrowClick(this.code);
+    }
 };
 
-ShortInfo.prototype.setEffect = function(effect)
-{
-    this.effect = effect;
-};
+var ShortInfo       = ShortInfoPrototype.construct;
+ShortInfo.prototype = ShortInfoPrototype;
+ShortInfo.prototype.construct = null;
